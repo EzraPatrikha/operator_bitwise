f2b78c89448d3a1a093bea889a25ba14cfcbc776
Ezra <ezra_chandra_32rpl@student.smktelkom-mlg.sch.id>
2023-08-24 11:54:35 +0700

    menambah tes tes tes blabal

--- operator_bitwise.java
+++ operator_bitwise.java
@@ -1,7 +1,7 @@
 public class Praktikum_Tipe_Data_2 {
     
     public static void main(String[] args) {
-        String nama = "Tes";
+        String nama = "Tes Tes TEs Tes 1234";
         System.out.println("Nama Saya : "+nama);
     }
 }
